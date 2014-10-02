6e70dcf44b087967f68a05d31b8c7c92eea7ba4f
Kirill Alexandrovsky <k.alexandrovsky@gmail.com>
2014-10-03 02:19:56 +0400

    introducing rdo.lib notification system
    quite a simple one, this

---
+++ ru.bmstu.rk9.rdo.lib/src/ru/bmstu/rk9/rdo/lib/NotificationManager.java
@@ -0,0 +1,33 @@
+package ru.bmstu.rk9.rdo.lib;
+
+import java.util.HashMap;
+
+public class NotificationManager implements Notifier
+{
+	protected HashMap<String, Notifier.Subscription> subscribtions;
+
+	public NotificationManager(String[] categories)
+	{
+		subscribtions = new HashMap<String, Notifier.Subscription>();
+		for(String s : categories)
+			subscribtions.put(s, new Notifier.Subscription());
+	}
+
+	public void notifySubscribers(String category)
+	{
+		for(Subscriber s : subscribtions.get(category).subscribers)
+			s.fireChange();
+	}
+
+	@Override
+	public Notifier.Subscription getSubscription(String category)
+	{
+		return subscribtions.get(category);
+	}
+
+	@Override
+	public String[] getAvailableSubscriptions()
+	{
+		return (String[]) subscribtions.keySet().toArray();
+	}
+}
---
+++ ru.bmstu.rk9.rdo.lib/src/ru/bmstu/rk9/rdo/lib/Subscriber.java
@@ -0,0 +1,6 @@
+package ru.bmstu.rk9.rdo.lib;
+
+public interface Subscriber
+{
+	public void fireChange();
+}
