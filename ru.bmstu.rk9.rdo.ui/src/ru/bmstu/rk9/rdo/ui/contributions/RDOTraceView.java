4d64781c80b7f1ad66076a278fa8ccaf9dc417c1
bogachev-pa <bogachev.pa@gmail.com>
2014-10-29 11:25:57 +0400

    RDOTraceView colors conforming with old rdo colors (#3)

--- ru.bmstu.rk9.rdo.ui/src/ru/bmstu/rk9/rdo/ui/contributions/RDOTraceView.java
+++ ru.bmstu.rk9.rdo.ui/src/ru/bmstu/rk9/rdo/ui/contributions/RDOTraceView.java
@@ -29,70 +29,14 @@ public class RDOTraceView extends ViewPart
 
 	static TableViewer viewer;
 
-	public static RDOTraceUpdater updater;
-
-	public static final EnumMap<Tracer.TraceType, Color> colorByType =
-		new EnumMap<Tracer.TraceType, Color>(Tracer.TraceType.class);
-
 	@Override
 	public void createPartControl(Composite parent)
 	{
 		createViewer(parent);
 	}
 
-	private final void initializeColorMap()
-	{
-		//TODO choose proper colors
-		//customs colors need to be created and disposed of
-		colorByType.put(
-			Tracer.TraceType.RESOURCE_CREATE,
-			PlatformUI.getWorkbench().getDisplay().getSystemColor(SWT.COLOR_CYAN)
-		);
-
-		colorByType.put(
-			Tracer.TraceType.RESOURCE_KEEP,
-			PlatformUI.getWorkbench().getDisplay().getSystemColor(SWT.COLOR_GREEN)
-		);
-
-		colorByType.put(
-			Tracer.TraceType.RESOURCE_ERASE,
-			PlatformUI.getWorkbench().getDisplay().getSystemColor(SWT.COLOR_DARK_CYAN)
-		);
-
-		colorByType.put(
-			Tracer.TraceType.SYSTEM,
-			PlatformUI.getWorkbench().getDisplay().getSystemColor(SWT.COLOR_RED)
-		);
-
-		colorByType.put(
-			Tracer.TraceType.OPERATION_BEGIN,
-			PlatformUI.getWorkbench().getDisplay().getSystemColor(SWT.COLOR_BLUE)
-		);
-
-		colorByType.put(
-			Tracer.TraceType.OPERATION_END,
-			PlatformUI.getWorkbench().getDisplay().getSystemColor(SWT.COLOR_DARK_BLUE)
-		);
-
-		colorByType.put(
-			Tracer.TraceType.EVENT,
-			PlatformUI.getWorkbench().getDisplay().getSystemColor(SWT.COLOR_YELLOW)
-		);
-
-		colorByType.put(
-			Tracer.TraceType.RULE,
-			Display.getCurrent().getSystemColor(SWT.COLOR_GRAY)
-		);
-
-		colorByType.put(
-			Tracer.TraceType.RESULT,
-			Display.getCurrent().getSystemColor(SWT.COLOR_WHITE)
-		);
-	}
-
 	private final void createViewer(Composite parent)
 	{
-		initializeColorMap();
 		viewer = new TableViewer(
 			parent,
 			SWT.H_SCROLL | SWT.V_SCROLL | SWT.VIRTUAL
@@ -113,8 +57,7 @@ public class RDOTraceView extends ViewPart
 			fontRegistry.get(PreferenceConstants.EDITOR_TEXT_FONT));
 	}
 
-	@Override
-	public void setFocus() {}
+	public static RDOTraceUpdater updater;
 
 	public class RDOTraceUpdater implements Subscriber
 	{
@@ -136,6 +79,9 @@ public class RDOTraceView extends ViewPart
 			);
 		}
 	}
+
+	@Override
+	public void setFocus() {}
 }
 
 class RDOTraceViewContentProvider implements ILazyContentProvider
@@ -163,11 +109,124 @@ class RDOTraceViewContentProvider implements ILazyContentProvider
 // but since there is only one column, currently there is no difference
 class RDOTraceViewLabelProvider implements ILabelProvider, IColorProvider
 {
+	private final EnumMap<Tracer.TraceType, TraceColor> colorByType =
+		new EnumMap<Tracer.TraceType, TraceColor>(Tracer.TraceType.class);
+
+	RDOTraceViewLabelProvider()
+	{
+		initializeColorMap();
+	}
+
+	private class TraceColor
+	{
+		private final Color foregroundColor;
+		private final Color backgroundColor;
+
+		public TraceColor(Color fg, Color bg)
+		{
+			foregroundColor = fg;
+			backgroundColor = bg;
+		}
+
+		public final Color foregroundColor()
+		{
+			return foregroundColor;
+		}
+
+		public final Color backgroundColor()
+		{
+			return backgroundColor;
+		}
+	}
+
+	private final void initializeColorMap()
+	{
+		Display display = PlatformUI.getWorkbench().getDisplay();
+		colorByType.put(
+			Tracer.TraceType.RESOURCE_CREATE,
+			new TraceColor(
+				new Color(display, 0x23, 0x74, 0x42),
+				new Color(display, 0x96, 0xFF, 0x96)
+			)
+		);
+
+		colorByType.put(
+			Tracer.TraceType.RESOURCE_KEEP,
+			new TraceColor(
+				new Color(display, 0x00, 0x86, 0x00),
+				new Color(display, 0xD0, 0xFF, 0xD0)
+			)
+		);
+
+		colorByType.put(
+			Tracer.TraceType.RESOURCE_ERASE,
+			new TraceColor(
+				new Color(display, 0x43, 0x5A, 0x43),
+				new Color(display, 0xB4, 0xE0, 0xB4)
+			)
+		);
+
+		colorByType.put(
+			Tracer.TraceType.SYSTEM,
+			new TraceColor(
+				new Color(display, 0x8B, 0x00, 0x00),
+				new Color(display, 0xFF, 0xC0, 0xCB)
+			)
+		);
+
+		colorByType.put(
+			Tracer.TraceType.OPERATION_BEGIN,
+			new TraceColor(
+				new Color(display, 0x34, 0x4B, 0xA2),
+				new Color(display, 0xAA, 0xE3, 0xFB)
+			)
+		);
+
+		colorByType.put(
+			Tracer.TraceType.OPERATION_END,
+			new TraceColor(
+				new Color(display, 0x16, 0x02, 0x50),
+				new Color(display, 0x81, 0xB0, 0xD5)
+			)
+		);
+
+		colorByType.put(
+			Tracer.TraceType.EVENT,
+			new TraceColor(
+				new Color(display, 0x4F, 0x29, 0x62),
+				new Color(display, 0xD0, 0xD0, 0xFF)
+			)
+		);
+
+		colorByType.put(
+			Tracer.TraceType.RULE,
+			new TraceColor(
+				new Color(display, 0x17, 0x32, 0x47),
+				new Color(display, 0xB6, 0xCB, 0xDB)
+			)
+		);
+
+		colorByType.put(
+			Tracer.TraceType.RESULT,
+			new TraceColor(
+				new Color(display, 0x00, 0x00, 0x00),
+				new Color(display, 0xF1, 0xFB, 0xE2)
+			)
+		);
+	}
+
 	@Override
 	public void addListener(ILabelProviderListener listener) {}
 
 	@Override
-	public void dispose() {}
+	public void dispose()
+	{
+		for (final TraceColor color : colorByType.values())
+		{
+			color.foregroundColor().dispose();
+			color.backgroundColor().dispose();
+		}
+	}
 
 	@Override
 	public boolean isLabelProperty(Object element, String property)
@@ -193,13 +252,14 @@ class RDOTraceViewLabelProvider implements ILabelProvider, IColorProvider
 	@Override
 	public Color getForeground(Object element)
 	{
-		return null;
+		Tracer.TraceType type = ((Tracer.TraceOutput) element).type();
+		return colorByType.get(type).foregroundColor();
 	}
 
 	@Override
 	public Color getBackground(Object element)
 	{
 		Tracer.TraceType type = ((Tracer.TraceOutput) element).type();
-		return RDOTraceView.colorByType.get(type);
+		return colorByType.get(type).backgroundColor();
 	}
 }
