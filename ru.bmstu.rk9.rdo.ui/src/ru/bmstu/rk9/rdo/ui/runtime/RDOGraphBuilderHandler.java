7b8d216ea9a2fccd587a861668769e14918569eb
kstrizhov <kstrizhov@gmail.com>
2014-12-01 14:47:09 +0300

    test button Build Graph was added

---
+++ ru.bmstu.rk9.rdo.ui/src/ru/bmstu/rk9/rdo/ui/runtime/RDOGraphBuilderHandler.java
@@ -0,0 +1,17 @@
+package ru.bmstu.rk9.rdo.ui.runtime;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+
+public class RDOGraphBuilderHandler extends AbstractHandler {
+		
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException
+	{
+		System.out.println("qqqqq");
+		
+		return null;
+    }
+
+}
