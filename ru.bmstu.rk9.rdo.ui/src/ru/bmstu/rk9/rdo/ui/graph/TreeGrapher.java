47f294a5d909fb59add46de1ee89ef191cd853cd
kstrizhov <kstrizhov@gmail.com>
2014-12-18 02:15:48 +0300

    some refactoring

--- ru.bmstu.rk9.rdo.ui/src/ru/bmstu/rk9/rdo/ui/graph/TreeGrapher.java
+++ ru.bmstu.rk9.rdo.ui/src/ru/bmstu/rk9/rdo/ui/graph/TreeGrapher.java
@@ -65,25 +65,101 @@ public class TreeGrapher {
 		final ArrayList<Database.Entry> entries = Simulator.getDatabase().getAllEntries();
 
 		for (Database.Entry entry : entries) {
-			parseSerializedData(entry);
-		}
-	}
+			final Database.EntryType type = Database.EntryType.values()[entry.getHeader().get(
+					TypeSize.Internal.ENTRY_TYPE_OFFSET)];
+			switch (type) {
+			case SEARCH:
+				
+				final ByteBuffer header = prepareBufferForReading(entry.getHeader());
+				final ByteBuffer data = prepareBufferForReading(entry.getData());
 
-	private void parseSerializedData(Database.Entry entry) {
-		final Database.EntryType type = Database.EntryType.values()[entry.getHeader().get(
-				TypeSize.Internal.ENTRY_TYPE_OFFSET)];
-		switch (type) {
-		case SYSTEM:
-			break;
-		case RESOURCE:
-			break;
-		case PATTERN:
-			break;
-		case SEARCH:
-			parseSearchEntry(entry);
-			break;
-		case RESULT:
-			break;
+				Node treeNode = new Node();
+				skipPart(header, TypeSize.BYTE);
+				final Database.SearchEntryType entryType = Database.SearchEntryType.values()[header.get()];
+				switch (entryType) {
+				case BEGIN: {
+					skipPart(data, TypeSize.DOUBLE);
+					final int dptNumber = data.getInt();
+					currentDptNumber = dptNumber;
+					solutionMap.put(currentDptNumber, new ArrayList<Integer>());
+					mapList.put(currentDptNumber, new HashMap<Integer, Node>());
+					treeNode.parent = null;
+					treeNode.nodeNumber = 0;
+					mapList.get(currentDptNumber).put(treeNode.nodeNumber, null);
+					treeNode.label = Integer.toString(treeNode.nodeNumber);
+					mapList.get(currentDptNumber).put(treeNode.nodeNumber, treeNode);
+					break;
+				}
+				case END: {
+					final DecisionPointSearch.StopCode endStatus = DecisionPointSearch.StopCode.values()[data.get()];
+					infoMap.put(currentDptNumber, new GraphInfo());
+					switch (endStatus) {
+					case SUCCESS: {
+						skipPart(data, TypeSize.DOUBLE + TypeSize.LONG * 2);
+						final double finalCost = data.getDouble();
+						final int totalOpened = data.getInt();
+						final int totalNodes = data.getInt();
+						infoMap.get(currentDptNumber).solutionCost = finalCost;
+						infoMap.get(currentDptNumber).numOpened = totalOpened;
+						infoMap.get(currentDptNumber).numNodes = totalNodes;
+						break;
+					}
+					case FAIL: {
+						skipPart(data, TypeSize.DOUBLE + TypeSize.LONG * 2 + TypeSize.DOUBLE);
+						final int totalOpened = data.getInt();
+						final int totalNodes = data.getInt();
+						infoMap.get(currentDptNumber).solutionCost = 0;
+						infoMap.get(currentDptNumber).numOpened = totalOpened;
+						infoMap.get(currentDptNumber).numNodes = totalNodes;
+						break;
+					}
+					default:
+						break;
+					}
+					currentDptNumber = -1;
+				}
+					break;
+				case OPEN:
+					break;
+				case SPAWN: {
+					final DecisionPointSearch.SpawnStatus spawnStatus = DecisionPointSearch.SpawnStatus.values()[data.get()];
+					switch (spawnStatus) {
+					case NEW: {
+						final int nodeNumber = data.getInt();
+						final int parentNumber = data.getInt();
+						treeNode.parent = mapList.get(currentDptNumber).get(parentNumber);
+						mapList.get(currentDptNumber).get(parentNumber).children.add(treeNode);
+						treeNode.nodeNumber = nodeNumber;
+						treeNode.label = Integer.toString(treeNode.nodeNumber);
+						mapList.get(currentDptNumber).put(nodeNumber, treeNode);
+						break;
+					}
+					case WORSE: {
+						break;
+					}
+					case BETTER: {
+						final int nodeNumber = data.getInt();
+						final int parentNumber = data.getInt();
+						treeNode.parent = mapList.get(currentDptNumber).get(parentNumber);
+						mapList.get(currentDptNumber).get(parentNumber).children.add(treeNode);
+						treeNode.nodeNumber = nodeNumber;
+						treeNode.label = Integer.toString(treeNode.nodeNumber);
+						mapList.get(currentDptNumber).put(nodeNumber, treeNode);
+						break;
+					}
+					}
+					break;
+				}
+				case DECISION: {
+					final int number = data.getInt();
+					solutionMap.get(currentDptNumber).add(number);
+					break;
+				}
+				}
+				break;
+			default:
+				break;
+			}
 		}
 	}
 
@@ -127,96 +203,6 @@ public class TreeGrapher {
 	}
 
 	private int currentDptNumber = -1;
-
-	private void parseSearchEntry(Entry entry) {
-
-		final ByteBuffer header = prepareBufferForReading(entry.getHeader());
-		final ByteBuffer data = prepareBufferForReading(entry.getData());
-
-		Node treeNode = new Node();
-		skipPart(header, TypeSize.BYTE);
-		final Database.SearchEntryType entryType = Database.SearchEntryType.values()[header.get()];
-		switch (entryType) {
-		case BEGIN: {
-			skipPart(data, TypeSize.DOUBLE);
-			final int dptNumber = data.getInt();
-			currentDptNumber = dptNumber;
-			solutionMap.put(currentDptNumber, new ArrayList<Integer>());
-			mapList.put(currentDptNumber, new HashMap<Integer, Node>());
-			treeNode.parent = null;
-			treeNode.nodeNumber = 0;
-			mapList.get(currentDptNumber).put(treeNode.nodeNumber, null);
-			treeNode.label = Integer.toString(treeNode.nodeNumber);
-			mapList.get(currentDptNumber).put(treeNode.nodeNumber, treeNode);
-			break;
-		}
-		case END: {
-			final DecisionPointSearch.StopCode endStatus = DecisionPointSearch.StopCode.values()[data.get()];
-			infoMap.put(currentDptNumber, new GraphInfo());
-			switch (endStatus) {
-			case SUCCESS: {
-				skipPart(data, TypeSize.DOUBLE + TypeSize.LONG * 2);
-				final double finalCost = data.getDouble();
-				final int totalOpened = data.getInt();
-				final int totalNodes = data.getInt();
-				infoMap.get(currentDptNumber).solutionCost = finalCost;
-				infoMap.get(currentDptNumber).numOpened = totalOpened;
-				infoMap.get(currentDptNumber).numNodes = totalNodes;
-				break;
-			}
-			case FAIL: {
-				skipPart(data, TypeSize.DOUBLE + TypeSize.LONG * 2 + TypeSize.DOUBLE);
-				final int totalOpened = data.getInt();
-				final int totalNodes = data.getInt();
-				infoMap.get(currentDptNumber).solutionCost = 0;
-				infoMap.get(currentDptNumber).numOpened = totalOpened;
-				infoMap.get(currentDptNumber).numNodes = totalNodes;
-				break;
-			}
-			default:
-				break;
-			}
-			currentDptNumber = -1;
-		}
-			break;
-		case OPEN:
-			break;
-		case SPAWN: {
-			final DecisionPointSearch.SpawnStatus spawnStatus = DecisionPointSearch.SpawnStatus.values()[data.get()];
-			switch (spawnStatus) {
-			case NEW: {
-				final int nodeNumber = data.getInt();
-				final int parentNumber = data.getInt();
-				treeNode.parent = mapList.get(currentDptNumber).get(parentNumber);
-				mapList.get(currentDptNumber).get(parentNumber).children.add(treeNode);
-				treeNode.nodeNumber = nodeNumber;
-				treeNode.label = Integer.toString(treeNode.nodeNumber);
-				mapList.get(currentDptNumber).put(nodeNumber, treeNode);
-				break;
-			}
-			case WORSE: {
-				break;
-			}
-			case BETTER: {
-				final int nodeNumber = data.getInt();
-				final int parentNumber = data.getInt();
-				treeNode.parent = mapList.get(currentDptNumber).get(parentNumber);
-				mapList.get(currentDptNumber).get(parentNumber).children.add(treeNode);
-				treeNode.nodeNumber = nodeNumber;
-				treeNode.label = Integer.toString(treeNode.nodeNumber);
-				mapList.get(currentDptNumber).put(nodeNumber, treeNode);
-				break;
-			}
-			}
-			break;
-		}
-		case DECISION: {
-			final int number = data.getInt();
-			solutionMap.get(currentDptNumber).add(number);
-			break;
-		}
-		}
-	}
 
 	private void buildTree(mxGraph graph, HashMap<Integer, Node> map, Node parent) {
 		mxCell child = (mxCell) graph.insertVertex(graph.getDefaultParent(), null, parent, 385, 100, 30, 30,
