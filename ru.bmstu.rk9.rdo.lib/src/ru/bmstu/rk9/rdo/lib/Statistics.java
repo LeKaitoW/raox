73ebd90dd920a829aa54e1e6467499179f23a572
Kirill Alexandrovsky <k.alexandrovsky@gmail.com>
2014-12-31 22:48:44 +0400

    statistics module rewritten and extended

--- ru.bmstu.rk9.rdo.lib/src/ru/bmstu/rk9/rdo/lib/Statistics.java
+++ ru.bmstu.rk9.rdo.lib/src/ru/bmstu/rk9/rdo/lib/Statistics.java
@@ -8,6 +8,82 @@ import java.util.function.Function;
 
 public class Statistics
 {
+	public static class Storeless
+	{
+		private int sum;
+
+		private double mean;
+		private double variance;
+
+		public void next(double value)
+		{
+			double lastMean = mean;
+	
+			mean = 1d / ++sum * (value - lastMean) + lastMean;
+			variance = variance + 1d * (value - lastMean) * (value - mean);
+		}
+
+		public double getMean()
+		{
+			return mean;
+		}
+
+		public double getStandartDeviation()
+		{
+			return Math.sqrt(variance / sum);
+		}
+
+		public double getCoefficientOfVariation()
+		{
+			return variance / sum / mean * 100d;
+		}
+
+		private double median;
+
+		public boolean initFromDatabase(Result result)
+		{
+			Database database = Simulator.getDatabase();
+			Database.Index resultIndex = database.resultIndex.get(result.getName());
+
+			if(resultIndex != null && resultIndex.entries.size() > 0)
+			{
+				Function<Integer, Double> getValue =
+					result.getData().getString("value_type").equals("real")
+						? i -> database.allEntries.get(i).data.getDouble(0)
+						: i -> (double)database.allEntries.get(i).data.getInt(0);
+
+				PriorityQueue<Integer> queue = new PriorityQueue<Integer>
+				(
+					resultIndex.entries.size(),
+					(a, b) -> getValue.apply(a).compareTo(getValue.apply(b))
+				);
+
+				queue.addAll(resultIndex.entries);
+
+				int size = queue.size(), value = -1;
+
+				Iterator<Integer> iter = queue.iterator();
+
+				for(int i = 0; i < size / 2; i++, value = iter.next());
+
+				if(size % 2 == 0)
+					median = (getValue.apply(value) +
+						getValue.apply(iter.next())) / 2; 
+				else
+					median = getValue.apply(iter.next());
+					
+				return true;
+			}
+
+			return false;
+		}
+
+		public double getMedian()
+		{
+			return median;
+		}
+	}
+
 	public static class WeightedStoreless
 	{
 		private boolean started = false;
@@ -84,29 +160,38 @@ public class Statistics
 				PriorityQueue<Integer> queue = new PriorityQueue<Integer>
 				(
 					resultIndex.entries.size(),
-					(a, b) -> getValue.apply(a).compareTo(getValue.apply(b))
+					(a, b) -> getValue.apply(resultIndex.entries.get(a))
+						.compareTo(getValue.apply(resultIndex.entries.get(b)))
 				);
 
-				queue.addAll(resultIndex.entries);
+				for(int i = 0; i < resultIndex.entries.size(); i++)
+					queue.add(i);
 
 				double tempSum = weightSum; 
 
-				int number = -1;
+				int number = -1, previousN = -1;
+				double weight = 0, previousW = 0;
 				Iterator<Integer> iterator = queue.iterator();
-				while(tempSum > weightSum / 2)
+				while(tempSum >= weightSum / 2)
 				{
+					previousN = number;
 					number = iterator.next();
 
-					double weight =
-						database.allEntries.get(number + 1).header
-							.getDouble(Database.TypeSize.Internal.TIME_OFFSET) -
-						database.allEntries.get(number).header
-							.getDouble(Database.TypeSize.Internal.TIME_OFFSET);
+					previousW = weight;
+					weight =
+						database.allEntries.get(resultIndex.entries.get(number + 1))
+							.header.getDouble(Database.TypeSize.Internal.TIME_OFFSET) -
+						database.allEntries.get(resultIndex.entries.get(number))
+							.header.getDouble(Database.TypeSize.Internal.TIME_OFFSET);
 
 					tempSum -= weight;
 				}
 
-				median = getValue.apply(number);
+				double value = getValue.apply(number);
+				double previous = getValue.apply(previousN);
+
+				median = 2d * (value - previous) / (weight + previousW) *
+					((previousW / 2 + weight) - (weightSum / 2 - tempSum)) + previous;
 
 				return true;
 			}
@@ -117,6 +202,63 @@ public class Statistics
 		public double getMedian()
 		{
 			return median;
+		}
+	}
+
+	public static class LogicStoreless
+	{
+		private double minFalse = Double.MAX_VALUE;
+		private double maxFalse = Double.MIN_VALUE;
+
+		private double minTrue = Double.MAX_VALUE;
+		private double maxTrue = Double.MIN_VALUE;
+		
+		private double timeFalse = 0;
+		private double timeTrue = 0;
+
+		public void addState(boolean value, double delta)
+		{
+			if(value)
+			{
+				if(delta > maxTrue)
+					maxTrue = delta;
+				if(delta < minTrue)
+					minTrue = delta;
+				timeTrue += delta;
+			}
+			else
+			{
+				if(delta > maxFalse)
+					maxFalse = delta;
+				if(delta < minFalse)
+					minFalse = delta;
+				timeFalse += delta;
+			}
+		}
+
+		public double getMinFalse()
+		{
+			return minFalse;
+		}
+
+		public double getMinTrue()
+		{
+			return minTrue;
+		}
+
+		public double getMaxFalse()
+		{
+			return maxFalse;
+		}
+
+		public double getMaxTrue()
+		{
+			return maxTrue;
+		}
+
+		public double getPercent()
+		{
+			return timeTrue / (timeFalse + timeTrue);
 		}
 	}
 }
