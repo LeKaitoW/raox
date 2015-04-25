3d803e71e2df20fcdbdba16e88c9f85194e59c14
kstrizhov <kstrizhov@gmail.com>
2015-04-25 17:48:00 +0300

    kstrizhov/rdo-xtext#13 graph can be opened from Collected Objects tree

--- ru.bmstu.rk9.rdo.ui/src/ru/bmstu/rk9/rdo/ui/contributions/RDOSerializedObjectsView.java
+++ ru.bmstu.rk9.rdo.ui/src/ru/bmstu/rk9/rdo/ui/contributions/RDOSerializedObjectsView.java
@@ -1,7 +1,12 @@
 package ru.bmstu.rk9.rdo.ui.contributions;
 
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 import java.net.URL;
+import java.util.Timer;
 import java.util.TimerTask;
+
+import javax.swing.JFrame;
 
 import org.eclipse.core.runtime.FileLocator;
 import org.eclipse.core.runtime.Platform;
@@ -12,11 +17,18 @@ import org.eclipse.jface.viewers.ILabelProviderListener;
 import org.eclipse.jface.viewers.ITreeContentProvider;
 import org.eclipse.jface.viewers.TreeViewer;
 import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Listener;
+import org.eclipse.swt.widgets.Menu;
+import org.eclipse.swt.widgets.MenuItem;
 import org.eclipse.swt.widgets.Tree;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.part.ViewPart;
@@ -27,6 +39,9 @@ import ru.bmstu.rk9.rdo.lib.CollectedDataNode.IndexType;
 import ru.bmstu.rk9.rdo.lib.CollectedDataNode.ResourceIndex;
 import ru.bmstu.rk9.rdo.lib.Simulator;
 import ru.bmstu.rk9.rdo.lib.Subscriber;
+import ru.bmstu.rk9.rdo.lib.TreeBuilder;
+import ru.bmstu.rk9.rdo.ui.graph.GraphControl;
+import ru.bmstu.rk9.rdo.ui.graph.GraphFrame;
 
 public class RDOSerializedObjectsView extends ViewPart {
 
@@ -39,10 +54,129 @@ public class RDOSerializedObjectsView extends ViewPart {
 		serializedObjectsTree.setLayoutData(new GridLayout());
 		serializedObjectsTree.setLinesVisible(true);
 
-		serializedObjectsTreeViewer
-				.setContentProvider(new RDOSerializedObjectsContentProvider());
-		serializedObjectsTreeViewer
-				.setLabelProvider(new RDOSerializedObjectsLabelProvider());
+		serializedObjectsTreeViewer.setContentProvider(new RDOSerializedObjectsContentProvider());
+		serializedObjectsTreeViewer.setLabelProvider(new RDOSerializedObjectsLabelProvider());
+
+		final Menu popupMenu = new Menu(serializedObjectsTreeViewer.getTree());
+		final MenuItem graphMenuItem = new MenuItem(popupMenu, SWT.CASCADE);
+		graphMenuItem.setText("Build graph");
+		serializedObjectsTree.setMenu(popupMenu);
+
+		popupMenu.addListener(SWT.Show, new Listener() {
+
+			@Override
+			public void handleEvent(Event arg0) {
+				// TODO Auto-generated method stub
+				boolean enabled = false;
+
+				final CollectedDataNode node = (CollectedDataNode) serializedObjectsTreeViewer.getTree().getSelection()[0]
+						.getData();
+				IndexType type = node.getIndex().getType();
+
+				if (type == IndexType.SEARCH) {
+					enabled = true;
+				}
+
+				graphMenuItem.setEnabled(enabled);
+			}
+		});
+
+		graphMenuItem.addSelectionListener(new SelectionListener() {
+
+			@Override
+			public void widgetSelected(SelectionEvent arg0) {
+				// TODO Auto-generated method stub
+				final CollectedDataNode node = (CollectedDataNode) serializedObjectsTreeViewer.getTree().getSelection()[0]
+						.getData();
+
+				int dptNum = node.getIndex().getNumber();
+				String frameName = node.getName();
+
+				if (!GraphControl.openedGraphMap.containsValue(dptNum)) {
+					TreeBuilder treeBuilder = Simulator.getTreeBuilder();
+					treeBuilder.buildTree();
+					GraphFrame graphFrame = new GraphFrame(dptNum);
+					GraphControl.openedGraphMap.put(dptNum, graphFrame);
+					graphFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+					graphFrame.setSize(800, 600);
+					graphFrame.setTitle(frameName);
+					graphFrame.setLocationRelativeTo(null);
+					graphFrame.setVisible(true);
+
+					boolean isFinished;
+
+					Simulator.getTreeBuilder().rwLock.readLock().lock();
+					try {
+						isFinished = Simulator.getTreeBuilder().dptSimulationInfoMap.get(dptNum).isFinished;
+					} finally {
+						Simulator.getTreeBuilder().rwLock.readLock().unlock();
+					}
+
+					if (!isFinished) {
+						TimerTask graphUpdateTask = graphFrame.getGraphFrameUpdateTimerTask();
+						Timer graphUpdateTimer = new Timer();
+						graphUpdateTimer.scheduleAtFixedRate(graphUpdateTask, 0, 10);
+
+						TimerTask graphFinTask = graphFrame.getGraphFrameFinTimerTask();
+						Timer graphFinTimer = new Timer();
+						graphFinTimer.scheduleAtFixedRate(graphFinTask, 0, 10);
+					}
+
+					graphFrame.addWindowListener(new WindowListener() {
+
+						@Override
+						public void windowOpened(WindowEvent e) {
+							// TODO Auto-generated method stub
+
+						}
+
+						@Override
+						public void windowIconified(WindowEvent e) {
+							// TODO Auto-generated method stub
+
+						}
+
+						@Override
+						public void windowDeiconified(WindowEvent e) {
+							// TODO Auto-generated method stub
+
+						}
+
+						@Override
+						public void windowDeactivated(WindowEvent e) {
+							// TODO Auto-generated method stub
+
+						}
+
+						@Override
+						public void windowClosing(WindowEvent e) {
+							// TODO Auto-generated method stub
+
+						}
+
+						@Override
+						public void windowClosed(WindowEvent e) {
+							// TODO Auto-generated method stub
+							// graphUpdateTimer.cancel();
+							GraphControl.openedGraphMap.remove(dptNum);
+							System.out.println("window closed");
+						}
+
+						@Override
+						public void windowActivated(WindowEvent e) {
+							// TODO Auto-generated method stub
+
+						}
+					});
+				}
+			}
+
+			@Override
+			public void widgetDefaultSelected(SelectionEvent arg0) {
+				// TODO Auto-generated method stub
+
+			}
+		});
 
 		if (Simulator.isInitialized()) {
 			commonUpdater.fireChange();
@@ -54,8 +188,7 @@ public class RDOSerializedObjectsView extends ViewPart {
 	}
 
 	public final static boolean readyForInput() {
-		return serializedObjectsTreeViewer != null
-				&& !serializedObjectsTreeViewer.getTree().isDisposed()
+		return serializedObjectsTreeViewer != null && !serializedObjectsTreeViewer.getTree().isDisposed()
 				&& serializedObjectsTreeViewer.getContentProvider() != null
 				&& serializedObjectsTreeViewer.getLabelProvider() != null;
 	}
@@ -71,22 +204,19 @@ public class RDOSerializedObjectsView extends ViewPart {
 
 	public static TimerTask getRealTimeUpdaterTask() {
 		return new TimerTask() {
-			private final Display display = PlatformUI.getWorkbench()
-					.getDisplay();
+			private final Display display = PlatformUI.getWorkbench().getDisplay();
 			private final Runnable updater = new Runnable() {
 				@Override
 				public void run() {
 					if (!readyForInput())
 						return;
-					RDOSerializedObjectsView.
-							serializedObjectsTreeViewer.refresh();
+					RDOSerializedObjectsView.serializedObjectsTreeViewer.refresh();
 				}
 			};
 
 			@Override
 			public void run() {
-				if (haveNewRealTimeData && readyForInput()
-						&& !display.isDisposed()) {
+				if (haveNewRealTimeData && readyForInput() && !display.isDisposed()) {
 					haveNewRealTimeData = false;
 					display.asyncExec(updater);
 				}
@@ -106,15 +236,9 @@ public class RDOSerializedObjectsView extends ViewPart {
 					if (!readyForInput())
 						return;
 
-					CollectedDataNode root = Simulator.getDatabase()
-							.getIndexHelper().getTree();
+					CollectedDataNode root = Simulator.getDatabase().getIndexHelper().getTree();
 
-					PlatformUI
-							.getWorkbench()
-							.getDisplay()
-							.asyncExec(
-									() -> serializedObjectsTreeViewer
-											.setInput(root));
+					PlatformUI.getWorkbench().getDisplay().asyncExec(() -> serializedObjectsTreeViewer.setInput(root));
 				}
 			});
 		}
@@ -159,8 +283,7 @@ class RDOSerializedObjectsContentProvider implements ITreeContentProvider {
 	}
 }
 
-class RDOSerializedObjectsLabelProvider implements ILabelProvider,
-		IColorProvider{
+class RDOSerializedObjectsLabelProvider implements ILabelProvider, IColorProvider {
 	@Override
 	public void addListener(ILabelProviderListener listener) {
 	}
@@ -186,22 +309,14 @@ class RDOSerializedObjectsLabelProvider implements ILabelProvider,
 		Display display = PlatformUI.getWorkbench().getDisplay();
 
 		if (index == null) {
-			url = FileLocator.find(
-					Platform.getBundle("ru.bmstu.rk9.rdo.ui"),
-					new org.eclipse.core.runtime.Path("icons/cross-small-white.png"),
-					null);
-		}
-		else if (index.getType() == IndexType.RESOURCE
-				&& ((ResourceIndex) index).isErased()) {
-			url = FileLocator.find(
-					Platform.getBundle("ru.bmstu.rk9.rdo.ui"),
-					new org.eclipse.core.runtime.Path("icons/cross.png"),
-					null);
+			url = FileLocator.find(Platform.getBundle("ru.bmstu.rk9.rdo.ui"), new org.eclipse.core.runtime.Path(
+					"icons/cross-small-white.png"), null);
+		} else if (index.getType() == IndexType.RESOURCE && ((ResourceIndex) index).isErased()) {
+			url = FileLocator.find(Platform.getBundle("ru.bmstu.rk9.rdo.ui"), new org.eclipse.core.runtime.Path(
+					"icons/cross.png"), null);
 		} else {
-			url = FileLocator.find(
-					Platform.getBundle("ru.bmstu.rk9.rdo.ui"),
-					new org.eclipse.core.runtime.Path("icons/globe-small.png"),
-					null);
+			url = FileLocator.find(Platform.getBundle("ru.bmstu.rk9.rdo.ui"), new org.eclipse.core.runtime.Path(
+					"icons/globe-small.png"), null);
 		}
 
 		ImageDescriptor img = ImageDescriptor.createFromURL(url);
@@ -218,8 +333,7 @@ class RDOSerializedObjectsLabelProvider implements ILabelProvider,
 	public Color getForeground(Object element) {
 		CollectedDataNode node = (CollectedDataNode) element;
 		AbstractIndex index = node.getIndex();
-		if (index != null && index.getType() == IndexType.RESOURCE
-				&& ((ResourceIndex) index).isErased()) {
+		if (index != null && index.getType() == IndexType.RESOURCE && ((ResourceIndex) index).isErased()) {
 			Display display = PlatformUI.getWorkbench().getDisplay();
 			return new Color(display, 0x88, 0x88, 0x88);
 		}
