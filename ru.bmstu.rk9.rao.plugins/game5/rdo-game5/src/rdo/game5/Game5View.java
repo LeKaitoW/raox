9f43adb80b91e3777bc0582e43b0e7b5fa3ea01c
LeKaitoW <le.chat.noir.kaito@gmail.com>
2015-06-28 20:57:08 +0300

    first commit

---
+++ ru.bmstu.rk9.rao.plugins/game5/rdo-game5/src/rdo/game5/Game5View.java
@@ -0,0 +1,18 @@
+package rdo.game5;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.ViewPart;
+
+public class Game5View extends ViewPart {
+	public static final String ID = "rdo-game5.Game5View";
+
+	@Override
+	public void createPartControl(Composite parent) {
+		
+	}
+
+	@Override
+	public void setFocus() {
+		
+	}
+}
