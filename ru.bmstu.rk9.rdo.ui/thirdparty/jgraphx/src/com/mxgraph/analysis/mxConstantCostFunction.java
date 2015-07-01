2fd68b5bfdfd07cd8cf692df132d66309be984ce
Andrey Urusov <drobus@gmail.com>
2015-07-02 00:10:17 +0300

    Full thirdpaty added

---
+++ ru.bmstu.rk9.rdo.ui/thirdparty/jgraphx/src/com/mxgraph/analysis/mxConstantCostFunction.java
@@ -0,0 +1,36 @@
+/**
+ * Copyright (c) 2007, Gaudenz Alder
+ */
+package com.mxgraph.analysis;
+
+import com.mxgraph.view.mxCellState;
+
+/**
+ * Implements a cost function for a constant cost per traversed cell.
+ */
+public class mxConstantCostFunction implements mxICostFunction
+{
+
+	/**
+	 * 
+	 */
+	protected double cost = 0;
+
+	/**
+	 * 
+	 * @param cost
+	 */
+	public mxConstantCostFunction(double cost)
+	{
+		this.cost = cost;
+	}
+
+	/**
+	 *
+	 */
+	public double getCost(mxCellState state)
+	{
+		return cost;
+	}
+
+}
