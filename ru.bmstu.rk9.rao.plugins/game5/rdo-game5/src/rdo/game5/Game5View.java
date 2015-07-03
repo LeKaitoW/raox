cb07624f7521a83efd3254e8336c344f6e622fd8
LeKaitoW <le.chat.noir.kaito@gmail.com>
2015-07-03 10:47:27 +0300

    add checkbox listeners add finals

--- ru.bmstu.rk9.rao.plugins/game5/rdo-game5/src/rdo/game5/Game5View.java
+++ ru.bmstu.rk9.rao.plugins/game5/rdo-game5/src/rdo/game5/Game5View.java
@@ -2,17 +2,24 @@ package rdo.game5;
 
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.layout.FormLayout;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IEditorSite;
 import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.part.EditorPart;
 
 public class Game5View extends EditorPart {
@@ -49,114 +56,204 @@ public class Game5View extends EditorPart {
 	@Override
 	public void createPartControl(Composite parent) {
 
-		GridLayout gridLayout = new GridLayout(3, false);
+		final GridLayout gridLayout = new GridLayout(3, false);
 		parent.setLayout(gridLayout);
 
-		Group solvableGroup = new Group(parent, SWT.SHADOW_IN);
+		final Group solvableGroup = new Group(parent, SWT.SHADOW_IN);
 		solvableGroup.setText("Solvable:");
 		solvableGroup.setLayout(gridLayout);
 
-		Button solvable = new Button(solvableGroup, SWT.CHECK);
+		final Button solvable = new Button(solvableGroup, SWT.CHECK);
 		solvable.setText("Solvable only");
 
-		Button inOrder = new Button(parent, SWT.PUSH);
+		final Button inOrder = new Button(parent, SWT.PUSH);
 		inOrder.setText("In order");
 
-		Group ruleCost = new Group(parent, SWT.SHADOW_IN);
+		final Group ruleCost = new Group(parent, SWT.SHADOW_IN);
 		ruleCost.setText("Rule cost:");
-		GridLayout ruleCostLayout = new GridLayout(4, false);
+		final GridLayout ruleCostLayout = new GridLayout(4, false);
 		ruleCost.setLayout(ruleCostLayout);
-		GridData ruleCostData = new GridData();
+		final GridData ruleCostData = new GridData();
 		ruleCostData.verticalSpan = 3;
 		ruleCost.setLayoutData(ruleCostData);
 
-		Label moveLabel = new Label(ruleCost, SWT.NONE);
+		final Label moveLabel = new Label(ruleCost, SWT.NONE);
 		moveLabel.setText("Move");
 
-		Label computeLabel = new Label(ruleCost, SWT.NONE);
+		final Label computeLabel = new Label(ruleCost, SWT.NONE);
 		computeLabel.setText("Compute");
 
-		Label costLabel = new Label(ruleCost, SWT.NONE);
+		final Label costLabel = new Label(ruleCost, SWT.NONE);
 		costLabel.setText("Cost");
-		GridData costLabelData = new GridData(SWT.FILL, SWT.BEGINNING, true,
-				false);
+		final GridData costLabelData = new GridData(SWT.FILL, SWT.BEGINNING,
+				true, false);
 		costLabelData.horizontalSpan = 2;
 		costLabel.setLayoutData(costLabelData);
 
-		Label leftLabel = new Label(ruleCost, SWT.NONE);
+		final Label leftLabel = new Label(ruleCost, SWT.NONE);
 		leftLabel.setText("Left");
 
-		Combo leftCombo = new Combo(ruleCost, SWT.BORDER | SWT.DROP_DOWN
+		final Combo leftCombo = new Combo(ruleCost, SWT.BORDER | SWT.DROP_DOWN
 				| SWT.V_SCROLL | SWT.READ_ONLY);
 		leftCombo.add("after");
 		leftCombo.add("before");
 		leftCombo.select(0);
 
-		Button leftButton = new Button(ruleCost, SWT.CHECK);
-		Text leftCost = new Text(ruleCost, SWT.BORDER);
+		final Button leftButton = new Button(ruleCost, SWT.CHECK);
+		final Text leftCost = new Text(ruleCost, SWT.BORDER);
+		leftCost.setEnabled(false);
 
-		Label rightLabel = new Label(ruleCost, SWT.NONE);
+		leftButton.addSelectionListener(new SelectionListener() {
+
+			@Override
+			public void widgetSelected(SelectionEvent arg0) {
+				if (leftButton.getSelection()) {
+					leftCost.setEnabled(true);
+				} else {
+					leftCost.setEnabled(false);
+				}
+
+			}
+
+			@Override
+			public void widgetDefaultSelected(SelectionEvent arg0) {
+			}
+		});
+
+		final Label rightLabel = new Label(ruleCost, SWT.NONE);
 		rightLabel.setText("Right");
 
-		Combo rightCombo = new Combo(ruleCost, SWT.BORDER | SWT.DROP_DOWN
+		final Combo rightCombo = new Combo(ruleCost, SWT.BORDER | SWT.DROP_DOWN
 				| SWT.V_SCROLL | SWT.READ_ONLY);
 		rightCombo.add("after");
 		rightCombo.add("before");
 		rightCombo.select(0);
 
-		Button rightButton = new Button(ruleCost, SWT.CHECK);
-		Text rightCost = new Text(ruleCost, SWT.BORDER);
+		final Button rightButton = new Button(ruleCost, SWT.CHECK);
+		final Text rightCost = new Text(ruleCost, SWT.BORDER);
+		rightCost.setEnabled(false);
 
-		Label upLabel = new Label(ruleCost, SWT.NONE);
+		rightButton.addSelectionListener(new SelectionListener() {
+
+			@Override
+			public void widgetSelected(SelectionEvent arg0) {
+				if (rightButton.getSelection()) {
+					rightCost.setEnabled(true);
+				} else {
+					rightCost.setEnabled(false);
+				}
+			}
+
+			@Override
+			public void widgetDefaultSelected(SelectionEvent arg0) {
+			}
+		});
+
+		final Label upLabel = new Label(ruleCost, SWT.NONE);
 		upLabel.setText("Up");
 
-		Combo upCombo = new Combo(ruleCost, SWT.BORDER | SWT.DROP_DOWN
+		final Combo upCombo = new Combo(ruleCost, SWT.BORDER | SWT.DROP_DOWN
 				| SWT.V_SCROLL | SWT.READ_ONLY);
 		upCombo.add("after");
 		upCombo.add("before");
 		upCombo.select(0);
 
-		Button upButton = new Button(ruleCost, SWT.CHECK);
-		Text upCost = new Text(ruleCost, SWT.BORDER);
+		final Button upButton = new Button(ruleCost, SWT.CHECK);
+		final Text upCost = new Text(ruleCost, SWT.BORDER);
+		upCost.setEnabled(false);
 
-		Label downLabel = new Label(ruleCost, SWT.NONE);
+		upButton.addSelectionListener(new SelectionListener() {
+
+			@Override
+			public void widgetSelected(SelectionEvent arg0) {
+				if (upButton.getSelection()) {
+					upCost.setEnabled(true);
+				} else {
+					upCost.setEnabled(false);
+				}
+			}
+
+			@Override
+			public void widgetDefaultSelected(SelectionEvent arg0) {
+			}
+		});
+
+		final Label downLabel = new Label(ruleCost, SWT.NONE);
 		downLabel.setText("Down");
 
-		Combo downCombo = new Combo(ruleCost, SWT.BORDER | SWT.DROP_DOWN
+		final Combo downCombo = new Combo(ruleCost, SWT.BORDER | SWT.DROP_DOWN
 				| SWT.V_SCROLL | SWT.READ_ONLY);
 		downCombo.add("after");
 		downCombo.add("before");
 		downCombo.select(0);
 
-		Group traverseGraph = new Group(parent, SWT.SHADOW_IN);
+		final Group traverseGraph = new Group(parent, SWT.SHADOW_IN);
 		traverseGraph.setText("Traverse graph:");
 		traverseGraph.setLayout(gridLayout);
 
-		Button compareTops = new Button(traverseGraph, SWT.CHECK);
+		final Button compareTops = new Button(traverseGraph, SWT.CHECK);
 		compareTops.setText("Compare tops");
 
-		Button shuffle = new Button(parent, SWT.PUSH);
+		final Button shuffle = new Button(parent, SWT.PUSH);
 		shuffle.setText("Shuffle");
 
-		Button downButton = new Button(ruleCost, SWT.CHECK);
+		final Button downButton = new Button(ruleCost, SWT.CHECK);
 		Text downCost = new Text(ruleCost, SWT.BORDER);
+		downCost.setEnabled(false);
 
-		Group heuristicSelection = new Group(parent, SWT.SHADOW_IN);
+		downButton.addSelectionListener(new SelectionListener() {
+
+			@Override
+			public void widgetSelected(SelectionEvent arg0) {
+				if (downButton.getSelection()) {
+					downCost.setEnabled(true);
+				} else
+					downCost.setEnabled(false);
+
+			}
+
+			@Override
+			public void widgetDefaultSelected(SelectionEvent arg0) {
+
+			}
+		});
+
+		final Group heuristicSelection = new Group(parent, SWT.SHADOW_IN);
 		heuristicSelection.setText("Select heuristic:");
 		heuristicSelection.setLayout(gridLayout);
-		Combo heuristicList = new Combo(heuristicSelection, SWT.BORDER
+		final Combo heuristicList = new Combo(heuristicSelection, SWT.BORDER
 				| SWT.DROP_DOWN | SWT.V_SCROLL);
 
-		Button setSituation = new Button(parent, SWT.PUSH);
+		final Button setSituation = new Button(parent, SWT.PUSH);
 		setSituation.setText("Set...");
 
-		Group heuristicCodeGroup = new Group(parent, SWT.SHADOW_IN);
+		setSituation.addSelectionListener(new SelectionListener() {
+
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				Display display = PlatformUI.getWorkbench().getDisplay();
+				Shell shell = new Shell(display);
+				shell.setText("Set situation");
+				shell.setLayout(new FormLayout());
+				shell.setLayout(gridLayout);
+
+				Text setText = new Text(shell, SWT.BORDER);
+				shell.setSize(180, 80);
+				shell.open();
+			}
+
+			@Override
+			public void widgetDefaultSelected(SelectionEvent e) {
+			}
+		});
+
+		final Group heuristicCodeGroup = new Group(parent, SWT.SHADOW_IN);
 		heuristicCodeGroup.setText("Heuristic code:");
 		heuristicCodeGroup.setLayout(gridLayout);
-		Text heuristicCode = new Text(heuristicCodeGroup, SWT.MULTI
+		final Text heuristicCode = new Text(heuristicCodeGroup, SWT.MULTI
 				| SWT.V_SCROLL);
-		GridData heuristicCodeData = new GridData(SWT.FILL, SWT.FILL, true,
-				true, 1, 1);
+		final GridData heuristicCodeData = new GridData(SWT.FILL, SWT.FILL,
+				true, true, 1, 1);
 		heuristicCodeData.horizontalSpan = 3;
 		heuristicCodeGroup.setLayoutData(heuristicCodeData);
 		heuristicCode.setLayoutData(heuristicCodeData);
