84bcc5c53a57748396004cf68d1796fc5ba327e5
LeKaitoW <le.chat.noir.kaito@gmail.com>
2015-08-03 23:42:29 +0300

    fix shuffle condition

--- ru.bmstu.rk9.rao.plugins/game5/rdo-game5/src/rdo/game5/OrderConfigurator.java
+++ ru.bmstu.rk9.rao.plugins/game5/rdo-game5/src/rdo/game5/OrderConfigurator.java
@@ -32,17 +32,8 @@ public class OrderConfigurator {
 		Collections.shuffle(places);
 		final int tilesCountX = 3;
 		final int tilesCountY = 2;
-		final int freePlaceRow = places.indexOf(String.valueOf(6))
-				/ tilesCountX + 1;
-		int sum = 0;
-		for (int i = 1; i < tilesCountX * tilesCountY; i++) {
-			for (int j = i + 1; j < tilesCountX * tilesCountY; j++) {
-				if (Integer.valueOf(places.get(i).toString()) > Integer
-						.valueOf(places.get(j).toString()))
-					sum++;
-			}
-		}
-		while (!isSolvable(tilesCountX, tilesCountY, sum, freePlaceRow)) {
+
+		while (!isSolvable(tilesCountX, tilesCountY, places)) {
 			Collections.shuffle(places);
 		}
 		return places;
@@ -54,7 +45,19 @@ public class OrderConfigurator {
 	}
 
 	private static boolean isSolvable(int tilesCountX, int tilesCountY,
-			int sum, int freePlaceRow) {
+			JSONArray places) {
+
+		final int freePlaceRow = places.indexOf(String.valueOf(6))
+				/ tilesCountX + 1;
+		int sum = 0;
+		for (int i = 1; i < tilesCountX * tilesCountY; i++) {
+			for (int j = i + 1; j < tilesCountX * tilesCountY; j++) {
+				if (Integer.valueOf(places.get(i).toString()) > Integer
+						.valueOf(places.get(j).toString()))
+					sum++;
+			}
+		}
+
 		boolean solvable = false;
 		if (tilesCountX % 2 != 0) {
 			if (sum % 2 == 0)
