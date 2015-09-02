cfae27f25e70a5e9d048659df6a890a98acf6090
LeKaitoW <le.chat.noir.kaito@gmail.com>
2015-09-02 20:57:13 +0300

    remove warnings (LeKaitoW/rdo-game5#4) remove action and actionSets add menu and command

---
+++ ru.bmstu.rk9.rao.plugins/game5/rdo-game5/src/rdo/game5/Game5Handler.java
@@ -0,0 +1,15 @@
+package rdo.game5;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+
+public class Game5Handler extends AbstractHandler {
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		ModelNameView.modelNameView();
+		return null;
+	}
+
+}
