7f8a8200cd127662b7db06c72358cadf3991807a
LeKaitoW <le.chat.noir.kaito@gmail.com>
2015-09-18 00:02:49 +0300

    add game 5 wizard

--- ru.bmstu.rk9.rao.plugins/game5/rdo-game5/src/rdo/game5/Game5Action.java
+++ ru.bmstu.rk9.rao.plugins/game5/rdo-game5/src/rdo/game5/Game5Action.java
@@ -2,8 +2,10 @@ package rdo.game5;
 
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.wizard.WizardDialog;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.IWorkbenchWindowActionDelegate;
+import org.eclipse.ui.PlatformUI;
 
 public class Game5Action implements IWorkbenchWindowActionDelegate {
 
@@ -11,7 +13,9 @@ public class Game5Action implements IWorkbenchWindowActionDelegate {
 	}
 
 	public void run(IAction action) {
-		ModelNameView.modelNameView();
+		WizardDialog wizardDialog = new WizardDialog(PlatformUI.getWorkbench()
+				.getActiveWorkbenchWindow().getShell(), new Game5Wizard());
+		wizardDialog.open();
 	}
 
 	public void selectionChanged(IAction action, ISelection selection) {
