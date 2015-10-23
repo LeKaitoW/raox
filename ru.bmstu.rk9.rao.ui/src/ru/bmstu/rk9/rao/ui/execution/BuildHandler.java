2083ff74578e1d7eba371d728487b93548127a42
bogachev-pa <bogachev.pa@gmail.com>
2015-10-23 22:13:47 +0300

    make BuildHandler and ExecutionStartHandler have common ancestor to handle ui elements updating

--- ru.bmstu.rk9.rao.ui/src/ru/bmstu/rk9/rao/ui/execution/BuildHandler.java
+++ ru.bmstu.rk9.rao.ui/src/ru/bmstu/rk9/rao/ui/execution/BuildHandler.java
@@ -1,30 +1,21 @@
 package ru.bmstu.rk9.rao.ui.execution;
 
-import java.util.Map;
-
-import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.core.resources.IProject;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IWorkbenchWindow;
-import org.eclipse.ui.PlatformUI;
-import org.eclipse.ui.commands.IElementUpdater;
 import org.eclipse.ui.handlers.HandlerUtil;
-import org.eclipse.ui.menus.UIElement;
 import org.eclipse.xtext.builder.EclipseOutputConfigurationProvider;
 import org.eclipse.xtext.builder.EclipseResourceFileSystemAccess2;
 import org.eclipse.xtext.ui.resource.IResourceSetProvider;
 import org.eclipse.xtext.ui.validation.DefaultResourceUIValidatorExtension;
 
 import ru.bmstu.rk9.rao.IMultipleResourceGenerator;
-import ru.bmstu.rk9.rao.ui.simulation.ModelExecutionSourceProvider;
-import ru.bmstu.rk9.rao.ui.simulation.ModelExecutionSourceProvider.SimulationState;
 
 import com.google.inject.Inject;
 import com.google.inject.Provider;
 
-public class BuildHandler extends AbstractHandler implements IElementUpdater {
+public class BuildHandler extends AbstractUIElementUpdatingHandler {
 	@Inject
 	private IMultipleResourceGenerator generator;
 
@@ -40,6 +31,10 @@ public class BuildHandler extends AbstractHandler implements IElementUpdater {
 	@Inject
 	DefaultResourceUIValidatorExtension validatorExtension;
 
+	public BuildHandler() {
+		super(UIElementType.BUILD);
+	}
+
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 		IEditorPart activeEditor = HandlerUtil.getActiveEditor(event);
@@ -52,43 +47,5 @@ public class BuildHandler extends AbstractHandler implements IElementUpdater {
 				validatorExtension);
 		executionManager.execute(true);
 		return null;
-	}
-
-	@SuppressWarnings("rawtypes")
-	@Override
-	public void updateElement(UIElement element, Map parameters) {
-		String message;
-
-		IWorkbenchWindow activeWorkbenchWindow = PlatformUI.getWorkbench()
-				.getActiveWorkbenchWindow();
-
-		if (activeWorkbenchWindow == null) {
-			ModelExecutionSourceProvider.setSimulationState(
-					activeWorkbenchWindow, SimulationState.DISABLED.toString());
-			message = "Build Rao model: no active workbench window";
-		} else {
-			IEditorPart activeEditor = activeWorkbenchWindow.getActivePage()
-					.getActiveEditor();
-
-			IProject projectToBuild = BuildJobProvider.getProjectToBuild(
-					activeWorkbenchWindow, activeEditor);
-
-			if (projectToBuild == null) {
-				ModelExecutionSourceProvider.setSimulationState(
-						activeWorkbenchWindow,
-						SimulationState.DISABLED.toString());
-				message = "Build Rao model: cannot choose project to build";
-			} else {
-				message = "Build model " + projectToBuild.getName();
-				if (ModelExecutionSourceProvider.getSimulationState().equals(
-						SimulationState.DISABLED.toString()))
-					ModelExecutionSourceProvider.setSimulationState(
-							activeWorkbenchWindow,
-							SimulationState.STOPPED.toString());
-			}
-		}
-
-		element.setText(message);
-		element.setTooltip(message);
 	}
 }
--- ru.bmstu.rk9.rao.ui/src/ru/bmstu/rk9/rao/ui/execution/StartExecutionHandler.java
+++ ru.bmstu.rk9.rao.ui/src/ru/bmstu/rk9/rao/ui/execution/StartExecutionHandler.java
@@ -1,31 +1,21 @@
 package ru.bmstu.rk9.rao.ui.execution;
 
-import java.util.Map;
-
-import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.core.resources.IProject;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IWorkbenchWindow;
-import org.eclipse.ui.PlatformUI;
-import org.eclipse.ui.commands.IElementUpdater;
 import org.eclipse.ui.handlers.HandlerUtil;
-import org.eclipse.ui.menus.UIElement;
 import org.eclipse.xtext.builder.EclipseOutputConfigurationProvider;
 import org.eclipse.xtext.builder.EclipseResourceFileSystemAccess2;
 import org.eclipse.xtext.ui.resource.IResourceSetProvider;
 import org.eclipse.xtext.ui.validation.DefaultResourceUIValidatorExtension;
 
 import ru.bmstu.rk9.rao.IMultipleResourceGenerator;
-import ru.bmstu.rk9.rao.ui.simulation.ModelExecutionSourceProvider;
-import ru.bmstu.rk9.rao.ui.simulation.ModelExecutionSourceProvider.SimulationState;
 
 import com.google.inject.Inject;
 import com.google.inject.Provider;
 
-public class StartExecutionHandler extends AbstractHandler implements
-		IElementUpdater {
+public class StartExecutionHandler extends AbstractUIElementUpdatingHandler {
 	@Inject
 	private IMultipleResourceGenerator generator;
 
@@ -41,6 +31,10 @@ public class StartExecutionHandler extends AbstractHandler implements
 	@Inject
 	DefaultResourceUIValidatorExtension validatorExtension;
 
+	public StartExecutionHandler() {
+		super(UIElementType.EXECUTE);
+	}
+
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 		IEditorPart activeEditor = HandlerUtil.getActiveEditor(event);
@@ -54,43 +48,5 @@ public class StartExecutionHandler extends AbstractHandler implements
 		executionManager.execute(false);
 
 		return null;
-	}
-
-	@SuppressWarnings("rawtypes")
-	@Override
-	public void updateElement(UIElement element, Map parameters) {
-		String message;
-
-		IWorkbenchWindow activeWorkbenchWindow = PlatformUI.getWorkbench()
-				.getActiveWorkbenchWindow();
-
-		if (activeWorkbenchWindow == null) {
-			ModelExecutionSourceProvider.setSimulationState(
-					activeWorkbenchWindow, SimulationState.DISABLED.toString());
-			message = "Execute Rao model: no active workbench window";
-		} else {
-			IEditorPart activeEditor = activeWorkbenchWindow.getActivePage()
-					.getActiveEditor();
-
-			IProject projectToBuild = BuildJobProvider.getProjectToBuild(
-					activeWorkbenchWindow, activeEditor);
-
-			if (projectToBuild == null) {
-				ModelExecutionSourceProvider.setSimulationState(
-						activeWorkbenchWindow,
-						SimulationState.DISABLED.toString());
-				message = "Execute Rao model: cannot choose project to run";
-			} else {
-				message = "Execute model " + projectToBuild.getName();
-				if (ModelExecutionSourceProvider.getSimulationState().equals(
-						SimulationState.DISABLED.toString()))
-					ModelExecutionSourceProvider.setSimulationState(
-							activeWorkbenchWindow,
-							SimulationState.STOPPED.toString());
-			}
-		}
-
-		element.setText(message);
-		element.setTooltip(message);
 	}
 }
