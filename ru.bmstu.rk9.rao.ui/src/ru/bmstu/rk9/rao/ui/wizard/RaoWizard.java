4401f9f87326c39c3cec7ca07a779de719650a55
Alexander Rudykh <asquad3@gmail.com>
2015-11-08 23:00:17 +0300

    Offer to open Rao perspective, when you creating rao-project

--- ru.bmstu.rk9.rao.ui/src/ru/bmstu/rk9/rao/ui/wizard/RaoWizard.java
+++ ru.bmstu.rk9.rao.ui/src/ru/bmstu/rk9/rao/ui/wizard/RaoWizard.java
@@ -1,15 +1,18 @@
 package ru.bmstu.rk9.rao.ui.wizard;
 
+import org.eclipse.core.runtime.IConfigurationElement;
+import org.eclipse.core.runtime.IExecutableExtension;
 import org.eclipse.jface.viewers.IStructuredSelection;
-import org.eclipse.jface.wizard.Wizard;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchWizard;
+import org.eclipse.ui.wizards.newresource.BasicNewProjectResourceWizard;
 
 import ru.bmstu.rk9.rao.ui.wizard.ProjectConfigurator.ProjectWizardStatus;
 
-public class RaoWizard extends Wizard implements IWorkbenchWizard {
+public class RaoWizard extends BasicNewProjectResourceWizard implements IWorkbenchWizard, IExecutableExtension {
 
 	protected RaoWizardPage wizardPage;
+	private IConfigurationElement configElement;
 
 	@Override
 	public void addPages() {
@@ -19,12 +22,11 @@ public class RaoWizard extends Wizard implements IWorkbenchWizard {
 
 	@Override
 	public boolean performFinish() {
-		final ProjectInfo info = new ProjectInfo(wizardPage.getProjectName(),
-				wizardPage.getTemplate());
-		final ProjectWizardStatus projectWizardStatus = new ProjectConfigurator(
-				info).initializeProject();
+		final ProjectInfo info = new ProjectInfo(wizardPage.getProjectName(), wizardPage.getTemplate());
+		final ProjectWizardStatus projectWizardStatus = new ProjectConfigurator(info).initializeProject();
 		switch (projectWizardStatus) {
 		case SUCCESS:
+			updatePerspective(configElement);
 			return true;
 		case UNDEFINED_ERROR:
 			return false;
@@ -40,5 +42,10 @@ public class RaoWizard extends Wizard implements IWorkbenchWizard {
 	@Override
 	public String getWindowTitle() {
 		return "New Rao Project";
+	}
+
+	@Override
+	public void setInitializationData(IConfigurationElement cfig, String propertyName, Object data) {
+		configElement = cfig;
 	}
 }
