eca6945b661bada68d78d33628b2d6742d804b24
Kirill Alexandrovsky <k.alexandrovsky@gmail.com>
2015-12-07 00:53:08 +0300

    added classes for implicit importing of rao.lib constructs

---
+++ ru.bmstu.rk9.rao/src/ru/bmstu/rk9/rao/RaoImplicitlyImportedFeatures.java
@@ -0,0 +1,28 @@
+package ru.bmstu.rk9.rao;
+
+import java.util.List;
+
+import org.eclipse.xtext.xbase.scoping.batch.ImplicitlyImportedFeatures;
+
+import com.google.common.collect.Lists;
+
+import ru.bmstu.rk9.rao.lib.runtime.Simulation;
+
+@SuppressWarnings("restriction")
+public class RaoImplicitlyImportedFeatures extends ImplicitlyImportedFeatures {
+	@Override
+	protected List<Class<?>> getExtensionClasses() {
+		List<Class<?>> classes = Lists.<Class<?>> newArrayList(Simulation.class);
+
+		classes.addAll(super.getExtensionClasses());
+		return classes;
+	}
+
+	@Override
+	protected List<Class<?>> getStaticImportClasses() {
+		List<Class<?>> classes = Lists.<Class<?>> newArrayList(Simulation.class);
+
+		classes.addAll(super.getStaticImportClasses());
+		return classes;
+	}
+}
