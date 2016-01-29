31b745e053ecdd141f22aaf1400c85e6da92f848
bogachev-pa <bogachev.pa@gmail.com>
2016-01-29 19:20:49 +0300

    xbase integration: introduce generator sequences (aurusov/raox#290)

--- ru.bmstu.rk9.rao.lib/src/ru/bmstu/rk9/rao/lib/sequence/ListGenerator.java
+++ ru.bmstu.rk9.rao.lib/src/ru/bmstu/rk9/rao/lib/sequence/ListGenerator.java
@@ -2,19 +2,29 @@ package ru.bmstu.rk9.rao.lib.sequence;
 
 import java.util.List;
 
-import ru.bmstu.rk9.rao.lib.thirdparty.Generator;
-
-public class ListGenerator<T> extends Generator<T> {
+public class ListGenerator<T> extends InfiniteGenerator<T> {
 	private final List<T> elements;
 
 	public ListGenerator(List<T> elements) {
 		this.elements = elements;
 	}
 
-	protected void run() throws InterruptedException {
-		while (true)
-			for (T element : elements)
-				yield(element);
+	@Override
+	protected void generate() throws InterruptedException {
+		for (T element : elements)
+			yield(element);
 	}
 }
 
+class EasyGen extends InfiniteGenerator<Integer> {
+	@Override
+	protected void generate() throws InterruptedException {
+		yield(0);
+	}
+
+	InfiniteGenerator<Integer> lg = new InfiniteGenerator<Integer>() {
+		@Override
+		protected void generate() throws InterruptedException {
+		}
+	};
+}
--- ru.bmstu.rk9.rao.lib/src/ru/bmstu/rk9/rao/lib/sequence/ListSequence.java
+++ ru.bmstu.rk9.rao.lib/src/ru/bmstu/rk9/rao/lib/sequence/ListSequence.java
@@ -1,17 +1,9 @@
 package ru.bmstu.rk9.rao.lib.sequence;
 
-import java.util.Iterator;
 import java.util.List;
 
-public class ListSequence<T> implements ArbitraryTypeSequence<T> {
-	private final Iterator<T> iterator;
-
+public class ListSequence<T> extends GeneratorSequence<T> {
 	public ListSequence(List<T> enumerationList) {
-		this.iterator = new ListGenerator<T>(enumerationList).iterator();
-	}
-
-	@Override
-	public T next() {
-		return iterator.next();
+		super(new ListGenerator<T>(enumerationList));
 	}
 }
