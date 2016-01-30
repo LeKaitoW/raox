b315c2294cb15ca045ee766b5320dfaac8d43e09
bogachev-pa <bogachev.pa@gmail.com>
2016-01-31 02:00:19 +0300

    xbase integration: stub for extending xbase expressions

--- ru.bmstu.rk9.rao/src/ru/bmstu/rk9/rao/RaoRuntimeModule.java
+++ ru.bmstu.rk9.rao/src/ru/bmstu/rk9/rao/RaoRuntimeModule.java
@@ -1,8 +1,13 @@
 package ru.bmstu.rk9.rao;
 
+import org.eclipse.xtext.xbase.compiler.XbaseCompiler;
 import org.eclipse.xtext.xbase.scoping.batch.ImplicitlyImportedFeatures;
+import org.eclipse.xtext.xbase.typesystem.computation.XbaseTypeComputer;
 
 import com.google.inject.name.Names;
+
+import ru.bmstu.rk9.rao.compiler.RaoCompiler;
+import ru.bmstu.rk9.rao.typesystem.RaoTypeComputer;
 
 @SuppressWarnings("restriction")
 public class RaoRuntimeModule extends ru.bmstu.rk9.rao.AbstractRaoRuntimeModule {
@@ -13,6 +18,14 @@ public class RaoRuntimeModule extends ru.bmstu.rk9.rao.AbstractRaoRuntimeModule 
 				.annotatedWith(
 						Names.named(org.eclipse.xtext.scoping.impl.AbstractDeclarativeScopeProvider.NAMED_DELEGATE))
 				.to(RaoXImportSectionNamespaceScopeProvider.class);
+	}
+
+	public Class<? extends XbaseCompiler> bindXbaseCompiler() {
+		return RaoCompiler.class;
+	}
+
+	public Class<? extends XbaseTypeComputer> bindXbaseTypeComputer() {
+		return RaoTypeComputer.class;
 	}
 
 	public Class<? extends ImplicitlyImportedFeatures> bindImplicitlyImportedFeatures() {
