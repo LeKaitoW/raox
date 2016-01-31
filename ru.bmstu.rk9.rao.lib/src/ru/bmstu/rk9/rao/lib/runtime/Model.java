c3e31b38060b2106608c60f82736ca01c49cedf1
bogachev-pa <bogachev.pa@gmail.com>
2016-01-31 16:56:08 +0300

    xbase integration: introduce array() helper function (aurusov/raox#290)

--- ru.bmstu.rk9.rao.lib/src/ru/bmstu/rk9/rao/lib/runtime/Model.java
+++ ru.bmstu.rk9.rao.lib/src/ru/bmstu/rk9/rao/lib/runtime/Model.java
@@ -1,9 +1,18 @@
 package ru.bmstu.rk9.rao.lib.runtime;
 
+import java.util.ArrayList;
+
+import org.eclipse.xtext.xbase.lib.CollectionLiterals;
+
 import ru.bmstu.rk9.rao.lib.simulator.Simulator;
 
 public class Model {
 	public static final double getCurrentTime() {
 		return Simulator.getTime();
+	}
+
+	@SafeVarargs
+	public static <T> ArrayList<T> array(T... initial) {
+		return CollectionLiterals.newArrayList(initial);
 	}
 }
