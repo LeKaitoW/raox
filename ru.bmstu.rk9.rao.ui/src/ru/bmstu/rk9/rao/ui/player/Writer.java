8221a3ffbcf6c364ffbc10a858010b8ad2bf5b19
timyrka67 <timyrka67@gmail.com>
2016-03-19 19:07:26 +0300

    Dump model state resourses to JSON aurusov/raox#298

---
+++ ru.bmstu.rk9.rao.ui/src/ru/bmstu/rk9/rao/ui/player/Writer.java
@@ -0,0 +1,52 @@
+package ru.bmstu.rk9.rao.ui.player;
+
+import java.util.Arrays;
+import ru.bmstu.rk9.rao.lib.simulator.SimulatorSubscriberManager;
+import ru.bmstu.rk9.rao.lib.simulator.Simulator;
+import ru.bmstu.rk9.rao.lib.simulator.Simulator.ExecutionState;
+import ru.bmstu.rk9.rao.lib.simulator.SimulatorSubscriberManager.SimulatorSubscriberInfo;
+import ru.bmstu.rk9.rao.lib.json.JSONArray;
+import ru.bmstu.rk9.rao.lib.json.JSONObject;
+import ru.bmstu.rk9.rao.lib.notification.Subscriber;
+
+public class Writer {
+
+	public Writer() {
+		initializeSubscribers();
+	}
+
+	private final void initializeSubscribers() {
+		simulationSubscriberManager.initialize(
+				Arrays.asList(new SimulatorSubscriberInfo(stateStorageSubscriber, ExecutionState.STATE_CHANGED),
+							  new SimulatorSubscriberInfo(simulationEndSubscriber, ExecutionState.EXECUTION_COMPLETED)));
+
+	}
+
+	public class StateStorageSubscriber implements Subscriber {
+		public void fireChange() {
+			modelStateStorage.addModelState(Simulator.getModelState());
+			jsonModelStateArray.put(serializer.dumpResoursestoJSONobject());
+		}
+
+	}
+
+	public class SimulationEndSubscriber implements Subscriber {
+		public void fireChange() {
+			JSONObject jsonModelStateObject = new JSONObject();
+			jsonModelStateObject.put("Model state array", jsonModelStateArray);
+			serializer.dumpResoursestoJSONfile(jsonModelStateObject);
+		}
+
+	}
+
+	public final void deinitializeSubscribers() {
+		simulationSubscriberManager.deinitialize();
+	}
+
+	private final JSONArray jsonModelStateArray = new JSONArray();
+	public final SimulationEndSubscriber simulationEndSubscriber = new SimulationEndSubscriber();
+	private final Serializer serializer = new Serializer();
+	private final ModelStateStorage modelStateStorage = new ModelStateStorage();
+	private final StateStorageSubscriber stateStorageSubscriber = new StateStorageSubscriber();
+	private final SimulatorSubscriberManager simulationSubscriberManager = new SimulatorSubscriberManager();
+}
