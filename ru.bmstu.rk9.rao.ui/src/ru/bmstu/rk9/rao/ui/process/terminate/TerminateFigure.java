a6835aeb5bff34b24696886037449e14d6b61a22
Andrey Urusov <drobus@gmail.com>
2016-03-28 23:02:34 +0300

    Графический процесс: добавлен вывод дока для Terminate (aurusov/raox#317)

--- ru.bmstu.rk9.rao.ui/src/ru/bmstu/rk9/rao/ui/process/terminate/TerminateFigure.java
+++ ru.bmstu.rk9.rao.ui/src/ru/bmstu/rk9/rao/ui/process/terminate/TerminateFigure.java
@@ -1,6 +1,8 @@
 package ru.bmstu.rk9.rao.ui.process.terminate;
 
+import org.eclipse.draw2d.FigureListener;
 import org.eclipse.draw2d.Graphics;
+import org.eclipse.draw2d.IFigure;
 import org.eclipse.draw2d.geometry.PointList;
 import org.eclipse.draw2d.geometry.Rectangle;
 
@@ -9,29 +11,47 @@ import ru.bmstu.rk9.rao.ui.process.ProcessFigure;
 
 public class TerminateFigure extends ProcessFigure {
 
+	private final ProcessConnectionAnchor inputConnectionAnchor;
+
 	public TerminateFigure() {
 		super();
 
-		ProcessConnectionAnchor inputConnectionAnchor;
 		inputConnectionAnchor = new ProcessConnectionAnchor(this);
-		inputConnectionAnchor.offsetHorizontal = offset;
-		inputConnectionAnchor.offsetVertical = 35;
 		inputConnectionAnchors.add(inputConnectionAnchor);
 		connectionAnchors.put(Terminate.TERMINAL_IN, inputConnectionAnchor);
+
+		addFigureListener(new FigureListener() {
+			@Override
+			public void figureMoved(IFigure figure) {
+				Rectangle bounds = figure.getBounds();
+				inputConnectionAnchor.offsetHorizontal = offset;
+				inputConnectionAnchor.offsetVertical = bounds.height / 2 + offset;
+			}
+		});
 
 		label.setText(Terminate.name);
 	}
 
 	@Override
 	protected void drawShape(Graphics graphics) {
-		Rectangle rectangle = getBounds().getCopy();
-		PointList points = new PointList();
-		int centerY = rectangle.y + (rectangle.height + 2 * offset) / 2;
-		int xRight = rectangle.x + rectangle.width - offset;
+		Rectangle bounds = getBounds();
+		final int centerY = bounds.y + (bounds.height + 2 * offset) / 2;
+		final int xRight = bounds.x + bounds.width - offset;
 
-		points.addPoint(xRight, rectangle.y + 3 * offset);
-		points.addPoint(rectangle.x + offset, centerY);
-		points.addPoint(xRight, rectangle.y + rectangle.height - offset);
+		PointList points = new PointList();
+		points.addPoint(xRight, bounds.y + 3 * offset);
+		points.addPoint(bounds.x + offset, centerY);
+		points.addPoint(xRight, bounds.y + bounds.height - offset);
+
+		graphics.setBackgroundColor(getBackgroundColor());
 		graphics.fillPolygon(points);
+	}
+
+	@Override
+	protected void drawDocks(Graphics graphics) {
+		Rectangle bounds = getBounds();
+		final int dockCenterX = bounds.x + inputConnectionAnchor.offsetHorizontal;
+		final int dockCenterY = bounds.y + inputConnectionAnchor.offsetVertical;
+		drawDock(graphics, dockCenterX, dockCenterY);
 	}
 }
