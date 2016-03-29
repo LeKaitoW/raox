ca113eee0e38bb1d18d2e262f13a5633d48dfd8d
Andrey Urusov <drobus@gmail.com>
2016-03-29 23:04:43 +0300

    Графический процесс: добавлен вывод доков для Test (aurusov/raox#317)

--- ru.bmstu.rk9.rao.ui/src/ru/bmstu/rk9/rao/ui/process/test/TestFigure.java
+++ ru.bmstu.rk9.rao.ui/src/ru/bmstu/rk9/rao/ui/process/test/TestFigure.java
@@ -1,6 +1,8 @@
 package ru.bmstu.rk9.rao.ui.process.test;
 
+import org.eclipse.draw2d.FigureListener;
 import org.eclipse.draw2d.Graphics;
+import org.eclipse.draw2d.IFigure;
 import org.eclipse.draw2d.geometry.PointList;
 import org.eclipse.draw2d.geometry.Rectangle;
 
@@ -12,38 +14,48 @@ public class TestFigure extends ProcessFigure {
 	public TestFigure() {
 		super();
 
-		ProcessConnectionAnchor inputConnectionAnchor, trueOutputConnectionAnchor, falseOutputConnectionAnchor;
-		inputConnectionAnchor = new ProcessConnectionAnchor(this);
-		inputConnectionAnchor.offsetHorizontal = offset;
-		inputConnectionAnchor.offsetVertical = 35;
+		ProcessConnectionAnchor inputConnectionAnchor = new ProcessConnectionAnchor(this);
 		inputConnectionAnchors.add(inputConnectionAnchor);
 		connectionAnchors.put(Test.TERMINAL_IN, inputConnectionAnchor);
 
-		trueOutputConnectionAnchor = new ProcessConnectionAnchor(this);
-		trueOutputConnectionAnchor.offsetHorizontal = 45;
-		trueOutputConnectionAnchor.offsetVertical = 35;
+		ProcessConnectionAnchor trueOutputConnectionAnchor = new ProcessConnectionAnchor(this);
 		outputConnectionAnchors.add(trueOutputConnectionAnchor);
 		connectionAnchors.put(Test.TERMINAL_TRUE_OUT, trueOutputConnectionAnchor);
 
-		falseOutputConnectionAnchor = new ProcessConnectionAnchor(this);
-		falseOutputConnectionAnchor.offsetHorizontal = 25;
-		falseOutputConnectionAnchor.offsetVertical = 55;
+		ProcessConnectionAnchor falseOutputConnectionAnchor = new ProcessConnectionAnchor(this);
 		outputConnectionAnchors.add(falseOutputConnectionAnchor);
 		connectionAnchors.put(Test.TERMINAL_FALSE_OUT, falseOutputConnectionAnchor);
+
+		addFigureListener(new FigureListener() {
+			@Override
+			public void figureMoved(IFigure figure) {
+				Rectangle bounds = figure.getBounds();
+
+				inputConnectionAnchor.offsetHorizontal = offset - 1;
+				inputConnectionAnchor.offsetVertical = bounds.height / 2 + offset - 1;
+
+				trueOutputConnectionAnchor.offsetHorizontal = bounds.width - offset - 1;
+				trueOutputConnectionAnchor.offsetVertical = inputConnectionAnchor.offsetVertical;
+
+				falseOutputConnectionAnchor.offsetHorizontal = bounds.width / 2 - 1;
+				falseOutputConnectionAnchor.offsetVertical = bounds.height - offset - 1;
+			}
+		});
 
 		label.setText(Test.name);
 	}
 
 	@Override
 	protected void drawShape(Graphics graphics) {
-		Rectangle rectangle = getBounds().getCopy();
+		Rectangle bounds = getBounds();
 		PointList points = new PointList();
-		int xCenter = rectangle.x + (rectangle.width) / 2;
-		int yCenter = rectangle.y + (rectangle.height + 2 * offset) / 2;
-		points.addPoint(xCenter, rectangle.y + 3 * offset);
-		points.addPoint(rectangle.x + offset, yCenter);
-		points.addPoint(xCenter, rectangle.y + rectangle.height - offset);
-		points.addPoint(rectangle.x + rectangle.width - offset, yCenter);
+		final int xCenter = bounds.x + (bounds.width) / 2;
+		final int yCenter = bounds.y + (bounds.height + 2 * offset) / 2;
+		points.addPoint(xCenter, bounds.y + 3 * offset + dockSize / 2);
+		points.addPoint(bounds.x + bounds.width - offset - dockSize / 2, yCenter);
+		points.addPoint(xCenter, bounds.y + bounds.height - offset - dockSize / 2);
+		points.addPoint(bounds.x + offset + dockSize / 2, yCenter);
+		graphics.setBackgroundColor(getBackgroundColor());
 		graphics.fillPolygon(points);
 	}
 }
