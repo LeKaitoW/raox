5a2db6804455e2313cd4c208c292ad3830423eb4
Andrey Urusov <drobus@gmail.com>
2016-04-03 17:26:22 +0300

    Графический процесс: изменён дизайн Queue (aurusov/raox#321)

--- ru.bmstu.rk9.rao.ui/src/ru/bmstu/rk9/rao/ui/process/queue/QueueFigure.java
+++ ru.bmstu.rk9.rao.ui/src/ru/bmstu/rk9/rao/ui/process/queue/QueueFigure.java
@@ -4,9 +4,7 @@ import org.eclipse.draw2d.Figure;
 import org.eclipse.draw2d.FigureListener;
 import org.eclipse.draw2d.Graphics;
 import org.eclipse.draw2d.IFigure;
-import org.eclipse.draw2d.geometry.PointList;
 import org.eclipse.draw2d.geometry.Rectangle;
-import org.eclipse.swt.graphics.Color;
 
 import ru.bmstu.rk9.rao.ui.process.ProcessConnectionAnchor;
 import ru.bmstu.rk9.rao.ui.process.ProcessFigure;
@@ -17,33 +15,23 @@ public class QueueFigure extends ProcessFigure {
 
 		@Override
 		final protected void paintFigure(Graphics graphics) {
-			Rectangle bounds = getBounds();
-			PointList points = new PointList();
-			final int xLeft = bounds.x;
-			final int xRight = bounds.x + bounds.width;
-			final int yTop = bounds.y;
-			final int yBottom = bounds.y + bounds.height;
-			points.addPoint(xLeft, yTop);
-			points.addPoint(xRight, yTop);
-			points.addPoint(xRight, yBottom);
-			points.addPoint(xLeft, yBottom);
-			graphics.setBackgroundColor(getBackgroundColor());
-			graphics.fillPolygon(points);
+			Rectangle bounds = getBounds().getCopy();
+			final int border = Math.min(bounds.width, bounds.height) / 7;
+			bounds.shrink(0, border);
 
-			Color previousColor = graphics.getBackgroundColor();
-			graphics.setBackgroundColor(pageBackgroundColor);
-			final int width = 6;
-			final int offset = 5;
-			for (int i = 1; i < 4; i++) {
-				PointList internalPoints = new PointList();
-				internalPoints.addPoint(bounds.x + (i - 1) * offset + i * width, bounds.y + 1 * offset);
-				internalPoints.addPoint(bounds.x + (i - 1) * offset + (i + 1) * width, bounds.y + 1 * offset);
-				internalPoints.addPoint(bounds.x + (i - 1) * offset + (i + 1) * width,
-						bounds.y + bounds.height - 1 * offset);
-				internalPoints.addPoint(bounds.x + (i - 1) * offset + i * width, bounds.y + bounds.height - 1 * offset);
-				graphics.fillPolygon(internalPoints);
+			graphics.setBackgroundColor(getBackgroundColor());
+			graphics.fillRectangle(bounds.x, bounds.y, bounds.width, border);
+			graphics.fillRectangle(bounds.x, bounds.bottom() - border, bounds.width, border);
+
+			final int partitionWidth = border;
+			int partitionIndex = 0;
+			while (true) {
+				final int left = bounds.right() - partitionWidth - partitionIndex * partitionWidth * 2;
+				if (left < bounds.x + bounds.width / 5)
+					break;
+				graphics.fillRectangle(left, bounds.y + border, partitionWidth, bounds.height - border * 2);
+				++partitionIndex;
 			}
-			graphics.setBackgroundColor(previousColor);
 		}
 
 		private static IFigure create() {
