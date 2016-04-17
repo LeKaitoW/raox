6348a6dbc714fc563fb62efeb3c43d9eed3e6efb
Andrey Urusov <drobus@gmail.com>
2016-04-17 19:31:46 +0300

    DeleteCommand simplification (aurusov/raox#334)

--- ru.bmstu.rk9.rao.ui/src/ru/bmstu/rk9/rao/ui/process/ProcessDeletePolicy.java
+++ ru.bmstu.rk9.rao.ui/src/ru/bmstu/rk9/rao/ui/process/ProcessDeletePolicy.java
@@ -4,15 +4,13 @@ import org.eclipse.gef.commands.Command;
 import org.eclipse.gef.editpolicies.ComponentEditPolicy;
 import org.eclipse.gef.requests.GroupRequest;
 
+import ru.bmstu.rk9.rao.ui.gef.Node;
 import ru.bmstu.rk9.rao.ui.process.commands.DeleteCommand;
 
 public class ProcessDeletePolicy extends ComponentEditPolicy {
 
 	@Override
 	protected Command createDeleteCommand(GroupRequest deleteRequest) {
-		DeleteCommand command = new DeleteCommand();
-		command.setModel(getHost().getModel());
-		command.setParentModel(getHost().getParent().getModel());
-		return command;
+		return new DeleteCommand((Node) getHost().getModel(), (Node) getHost().getParent().getModel());
 	}
 }
--- ru.bmstu.rk9.rao.ui/src/ru/bmstu/rk9/rao/ui/process/commands/DeleteCommand.java
+++ ru.bmstu.rk9.rao.ui/src/ru/bmstu/rk9/rao/ui/process/commands/DeleteCommand.java
@@ -5,14 +5,19 @@ import java.util.List;
 
 import org.eclipse.gef.commands.Command;
 
+import ru.bmstu.rk9.rao.ui.gef.Node;
 import ru.bmstu.rk9.rao.ui.process.connection.Connection;
 import ru.bmstu.rk9.rao.ui.process.node.BlockNode;
-import ru.bmstu.rk9.rao.ui.process.node.Node;
 
 public class DeleteCommand extends Command {
 
 	private Node node;
 	private Node parentNode;
+
+	public DeleteCommand(Node node, Node parentNode) {
+		this.node = node;
+		this.parentNode = parentNode;
+	}
 
 	@Override
 	public void execute() {
@@ -24,20 +29,12 @@ public class DeleteCommand extends Command {
 		}
 	}
 
-	public void setModel(Object model) {
-		node = (Node) model;
-	}
-
-	public void setParentModel(Object model) {
-		parentNode = (Node) model;
-	}
-
 	@Override
 	public void undo() {
 		parentNode.addChild(node);
 	}
 
-	private void disconnect(List<Connection> connections) {
+	private final void disconnect(List<Connection> connections) {
 		if (connections.isEmpty())
 			return;
 
