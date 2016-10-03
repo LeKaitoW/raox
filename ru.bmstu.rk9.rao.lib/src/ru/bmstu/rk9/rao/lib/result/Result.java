0b06365a09ff34012ab2173a3797dc4f0a0655fe
Alex Chernov <chernovalexeyolegovich@gmail.com>
2016-10-03 11:26:59 +0300

    rename Value -> Result

--- ru.bmstu.rk9.rao.lib/src/ru/bmstu/rk9/rao/lib/result/Result.java
+++ ru.bmstu.rk9.rao.lib/src/ru/bmstu/rk9/rao/lib/result/Result.java
@@ -2,40 +2,40 @@ package ru.bmstu.rk9.rao.lib.result;
 
 import java.util.function.Supplier;
 
-public class Value<T> extends AbstractResult<T> {
+public class Result<T> extends AbstractResult<T> {
 
-	public Value(Supplier<T> evaluate, Supplier<Boolean> condition, ResultMode resultMode, Statistics<T> statistics) {
+	public Result(Supplier<T> evaluate, Supplier<Boolean> condition, ResultMode resultMode, Statistics<T> statistics) {
 		this.evaluate = evaluate;
 		this.condition = condition;
 		this.resultMode = resultMode;
 		this.statistics = statistics;
 	}
 
-	public Value(Supplier<T> evaluate, Supplier<Boolean> condition, ResultMode resultMode) {
+	public Result(Supplier<T> evaluate, Supplier<Boolean> condition, ResultMode resultMode) {
 		this(evaluate, condition, resultMode, new ValueStatistics<T>());
 	}
 
-	public Value(Supplier<T> evaluate, ResultMode resultMode, Statistics<T> statistics) {
+	public Result(Supplier<T> evaluate, ResultMode resultMode, Statistics<T> statistics) {
 		this(evaluate, () -> true, resultMode, statistics);
 	}
 
-	public Value(Supplier<T> evaluate, Supplier<Boolean> condition, Statistics<T> statistics) {
+	public Result(Supplier<T> evaluate, Supplier<Boolean> condition, Statistics<T> statistics) {
 		this(evaluate, condition, ResultMode.AUTO, statistics);
 	}
 
-	public Value(Supplier<T> evaluate, ResultMode resultMode) {
+	public Result(Supplier<T> evaluate, ResultMode resultMode) {
 		this(evaluate, () -> true, resultMode, new ValueStatistics<T>());
 	}
 
-	public Value(Supplier<T> evaluate, Statistics<T> statistics) {
+	public Result(Supplier<T> evaluate, Statistics<T> statistics) {
 		this(evaluate, () -> true, ResultMode.AUTO, statistics);
 	}
 
-	public Value(Supplier<T> evaluate, Supplier<Boolean> condition) {
+	public Result(Supplier<T> evaluate, Supplier<Boolean> condition) {
 		this(evaluate, condition, ResultMode.AUTO, new ValueStatistics<T>());
 	}
 
-	public Value(Supplier<T> evaluate) {
+	public Result(Supplier<T> evaluate) {
 		this(evaluate, () -> true, ResultMode.AUTO, new ValueStatistics<T>());
 	}
 
