1b90324a89bef06d0c5b1c7aa7ed1dea6c6a4a55
Alex Chernov <chernovalexeyolegovich@gmail.com>
2016-12-27 11:20:39 +0300

    Добавлен новый тип результатов без источника данных, и фабрика для создания результатов(aurusov/#560)

---
+++ ru.bmstu.rk9.rao.lib/src/ru/bmstu/rk9/rao/lib/result/AbstractResult.java
@@ -0,0 +1,26 @@
+package ru.bmstu.rk9.rao.lib.result;
+
+import ru.bmstu.rk9.rao.lib.json.JSONObject;
+import ru.bmstu.rk9.rao.lib.naming.RaoNameable;
+import ru.bmstu.rk9.rao.lib.simulator.CurrentSimulator;
+
+public class AbstractResult<T> extends RaoNameable {
+
+	public AbstractResult(Statistics<T> statistics) {
+		this.statistics = statistics;
+	}
+
+	public final JSONObject getData() {
+		JSONObject datasetData = new JSONObject();
+		datasetData.put("name", getName());
+		statistics.updateData(datasetData);
+		return datasetData;
+	};
+
+	public final void update(T value, double time) {
+		statistics.update(value, time);
+		CurrentSimulator.getDatabase().addResultEntry(this, value);
+	};
+
+	protected final Statistics<T> statistics;
+}
--- ru.bmstu.rk9.rao.ui/src/ru/bmstu/rk9/rao/ui/execution/ModelInternalsParser.java
+++ ru.bmstu.rk9.rao.ui/src/ru/bmstu/rk9/rao/ui/execution/ModelInternalsParser.java
@@ -41,7 +41,7 @@ import ru.bmstu.rk9.rao.lib.naming.NamingHelper;
 import ru.bmstu.rk9.rao.lib.pattern.Pattern;
 import ru.bmstu.rk9.rao.lib.process.Block;
 import ru.bmstu.rk9.rao.lib.resource.ComparableResource;
-import ru.bmstu.rk9.rao.lib.result.Result;
+import ru.bmstu.rk9.rao.lib.result.AbstractResult;
 import ru.bmstu.rk9.rao.lib.simulator.CurrentSimulator;
 import ru.bmstu.rk9.rao.lib.simulator.SimulatorInitializationInfo;
 import ru.bmstu.rk9.rao.lib.simulator.SimulatorPreinitializationInfo;
@@ -340,7 +340,7 @@ public class ModelInternalsParser {
 		}
 
 		for (Field field : modelClass.getDeclaredFields()) {
-			if (Result.class.isAssignableFrom(field.getType()))
+			if (AbstractResult.class.isAssignableFrom(field.getType()))
 				resultFields.add(field);
 		}
 
@@ -370,7 +370,7 @@ public class ModelInternalsParser {
 			InvocationTargetException, ClassNotFoundException, IOException, CoreException {
 		for (Field resultField : resultFields) {
 			resultField.setAccessible(true);
-			Result<?> result = (Result<?>) resultField.get(null);
+			AbstractResult<?> result = (AbstractResult<?>) resultField.get(null);
 
 			String name = NamingHelper.createFullNameForMember(resultField);
 			result.setName(name);
