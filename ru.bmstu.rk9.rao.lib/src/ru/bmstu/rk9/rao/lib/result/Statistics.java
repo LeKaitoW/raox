ad7f95d55cee4867990fc21393492e9b24a585bc
Alex Chernov <chernovalexeyolegovich@gmail.com>
2017-03-19 18:50:26 +0300

    Результатам добавлена подписка на завершение прогона, и метод prepareData(), который при этом вызывается

--- ru.bmstu.rk9.rao.lib/src/ru/bmstu/rk9/rao/lib/result/Statistics.java
+++ ru.bmstu.rk9.rao.lib/src/ru/bmstu/rk9/rao/lib/result/Statistics.java
@@ -5,4 +5,5 @@ import ru.bmstu.rk9.rao.lib.json.JSONObject;
 public abstract class Statistics<T> {
 	public abstract void updateData(JSONObject data);
 	public abstract void update(T value, double currentTime);
+	public void prepareData() {};
 }
